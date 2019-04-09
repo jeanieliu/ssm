02cd80c9a6a9e916c64818959961cef63f4c103b
jeanie <574156761@qq.com>
2019-04-09 11:05:10 +0800

    查找一条记录，添加一条记录

--- mybatis20190409/src/test/java/com/neuedu/dao/IUserDAOTest.java
+++ mybatis20190409/src/test/java/com/neuedu/dao/IUserDAOTest.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,15 +15,21 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class IUserDAOTest {
-
-    @Test
-    public void selectAll() throws IOException {
+    SqlSessionFactory sqlSessionFactory;
+    @Before
+    public void before() throws IOException {
         /*1:文件提取*/
         String resource="mybatis-config.xml";
         /*2：生成io流--inputString*/
         InputStream inputStream= Resources.getResourceAsStream(resource);
         /*3：SqlSessionFactory对象*/
-        SqlSessionFactory sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+
+    @Test
+    public void selectAll() throws IOException {
+
         /*4:生成sqlSession对象*/
         SqlSession session=sqlSessionFactory.openSession();
 
@@ -33,5 +40,45 @@ public class IUserDAOTest {
         System.out.println(list);
         /*7:关闭*/
         session.close();
+    }
+
+    @Test
+    public void selectById() {
+        /*4:生成sqlSession对象*/
+        SqlSession session=sqlSessionFactory.openSession();
+
+        /*5：生成接口对象*/
+        IUserDAO userDAO=session.getMapper(IUserDAO.class);
+        /*6:使用*/
+        User list=userDAO.selectById(2);
+        System.out.println(list);
+        /*7:关闭*/
+        session.close();
+
+    }
+
+    @Test
+    public void insertUser() {
+        User user=new User();
+        user.setUname("zhang");
+        user.setUpwd("123456");
+        user.setAddress("上海");
+        user.setTelephone("12457812457");
+        user.setUlevel(1);
+        /*4:生成sqlSession对象*/
+        /*  生成的SqlSession对象，自动提交关闭，默认false
+        解决方法：1）sqlSessionFactory.openSession(true)：自动提交*/
+        SqlSession session=sqlSessionFactory.openSession();
+
+        /*5：生成接口对象*/
+        IUserDAO userDAO=session.getMapper(IUserDAO.class);
+        /*6:使用*/
+       Integer i=userDAO.insertUser(user);
+       /*2）：手动提交    session.commit();*/
+       session.commit();
+        System.out.println(i);
+        /*7:关闭*/
+        session.close();
+
     }
 }
