c17a58649b661cf68b295069d98dd41a0b5773c4
jeanie <574156761@qq.com>
2019-04-15 09:12:41 +0800

    Spring的简单实现

---
+++ spring20190415/src/main/java/com/neuedu/entity/User.java
@@ -0,0 +1,8 @@
+package com.neuedu.entity;
+
+public class User {
+    public void  say(){
+        System.out.println("spring的IOC的简单实现");
+    }
+
+}
---
+++ spring20190415/src/test/java/com/neuedu/entity/UserTest.java
@@ -0,0 +1,20 @@
+package com.neuedu.entity;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+
+    @Test
+    public void usertest(){
+        /*  1:创建一个ApplicationContext对象*/
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring-config.xml");
+        /*2：User对象的创建:     context.getBean中的值为xml中bean标签的id值
+     */
+        User user=(User)context.getBean("user");
+
+        user.say();
+    }
+
+}
